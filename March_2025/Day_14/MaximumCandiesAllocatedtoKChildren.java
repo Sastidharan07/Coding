7e7f849dbe20d9affb38c1b33e6a9c198d15758e
Sastidharan T <sastidharan07@gmail.com>
2025-03-14 18:49:28 +0530

    Create MaximumCandiesAllocatedtoKChildren.java

---
+++ March_2025/Day_14/MaximumCandiesAllocatedtoKChildren.java
@@ -0,0 +1,32 @@
+//Question : https://leetcode.com/problems/maximum-candies-allocated-to-k-children/description/
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+
+        long sum = 0;
+        int min = 1;
+        for (int i = 0; i < candies.length; i++) {
+            sum = sum + candies[i];
+        }
+        if (sum < k) {
+            return 0;
+        }
+        int max = (int) (sum / k);
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (checkvalid(candies, mid, k)) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return max;
+    }
+
+    public boolean checkvalid(int[] candie, int val, long m) {
+        long count = 0;
+        for (int k = 0; k < candie.length; k++) {
+            count = count + candie[k] / val;
+        }
+        return count >= m;
+    }
+}
