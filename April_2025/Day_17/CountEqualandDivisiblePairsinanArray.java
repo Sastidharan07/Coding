39052308574d599e4d32fd0ab9fb1b7712b58b08
Sastidharan T <sastidharan07@gmail.com>
2025-04-17 19:48:11 +0530

    Create CountEqualandDivisiblePairsinanArray.java

---
+++ April_2025/Day_17/CountEqualandDivisiblePairsinanArray.java
@@ -0,0 +1,22 @@
+//Question : https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/description/
+class Solution {
+    public int countPairs(int[] nums, int k) {
+
+        int n = nums.length, cnt = 0 ;
+
+        for(int i=0; i<n-1; ++i) {
+            for(int j=i+1; j<n; ++j) {
+
+                if((i*j)%k == 0) {
+                    if(nums[i] == nums[j]) {
+                        ++cnt ;
+                    }
+                }
+
+            }
+        }
+
+        return cnt ;
+
+    }
+}
