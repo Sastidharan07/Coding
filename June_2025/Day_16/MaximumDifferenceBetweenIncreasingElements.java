35329310dc3b740be92e8464f2df120fc429d639
Sastidharan T <sastidharan07@gmail.com>
2025-06-16 09:42:01 +0530

    Create MaximumDifferenceBetweenIncreasingElements.java

---
+++ June_2025/Day_16/MaximumDifferenceBetweenIncreasingElements.java
@@ -0,0 +1,17 @@
+//Question : https://leetcode.com/problems/maximum-difference-between-increasing-elements/description/
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int maxDiff = -1;
+        int minVal = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > minVal) {
+                maxDiff = Math.max(maxDiff, nums[i] - minVal);
+            } else {
+                minVal = nums[i];
+            }
+        }
+
+        return maxDiff;
+    }
+}
