b23766f9264582f041a4517a6c0c2bc114bdc2e5
Sastidharan T <sastidharan07@gmail.com>
2025-07-04 09:17:07 +0530

    Create FirstMissingPositive.java

---
+++ leetcode problems/FirstMissingPositive.java
@@ -0,0 +1,20 @@
+//Question : https://leetcode.com/problems/first-missing-positive/
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        HashSet <Integer> set =new HashSet<>();
+        for(int i=0 ; i<n ;i++){
+            if(nums[i]>0){
+                set.add(nums[i]);
+            }
+        }
+        for(int i=1; i<=set.size()+1 ; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
