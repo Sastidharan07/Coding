00f26bf45769968f45f16b5fe49062a7738db91d
Sastidharan T <sastidharan07@gmail.com>
2025-07-05 19:39:57 +0530

    Create FindLuckyIntegerInAnArray.java

---
+++ July_2025/Day_05/FindLuckyIntegerInAnArray.java
@@ -0,0 +1,15 @@
+//Question : https://leetcode.com/problems/find-lucky-integer-in-an-array/description/
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] output = new int[501];
+        for( int value : arr ){
+            output[value] += 1;
+        }
+        for( int i = 500; i > 0; i-- ){
+            if( output[i] == i ){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
