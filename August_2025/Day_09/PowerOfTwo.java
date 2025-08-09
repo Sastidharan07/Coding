98a9fc2e779d8f0d367957c446d53eb3cc2cfbb1
Sastidharan T <sastidharan07@gmail.com>
2025-08-09 18:10:49 +0530

    Create PowerOfTwo.java

---
+++ August_2025/Day_09/PowerOfTwo.java
@@ -0,0 +1,6 @@
+//Question : https://leetcode.com/problems/power-of-two/description/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+}
