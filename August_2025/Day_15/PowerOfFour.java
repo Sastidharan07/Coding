ebe9f59cde5dac2e9e79091da5171c7b8cf487c5
Sastidharan T <sastidharan07@gmail.com>
2025-08-15 09:35:17 +0530

    Create PowerOfFour.java

---
+++ August_2025/Day_15/PowerOfFour.java
@@ -0,0 +1,10 @@
+//Question : https://leetcode.com/problems/power-of-four/description/
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        while(n%4==0){
+            n/=4;
+        }
+        return n==1;
+    }
+}
