ee403fe50afb9bb39e66a4aeb49b4b4f77b23f51
Sastidharan T <sastidharan07@gmail.com>
2025-08-19 10:08:49 +0530

    Create NumberofZeroFilledSubarrays.java

---
+++ August_2025/Day_19/NumberofZeroFilledSubarrays.java
@@ -0,0 +1,23 @@
+//Question : https://leetcode.com/problems/number-of-zero-filled-subarrays/description/
+class Solution {
+    public long zeroFilledSubarray(int[] Nums) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        for(int num:Nums){
+            nums.add(num);
+        }
+        nums.add(-1);
+        long count = 0;
+        int start = -1;
+        for(int i =0;i<nums.size();i++){
+            if(nums.get(i) == 0 && start==-1){
+                start = i;
+            }
+            if(nums.get(i)!=0 && start!=-1){
+                int n = i-start;
+                start = -1;
+                count += (long)n*(n+1)/2;
+            }
+        }
+        return count;
+    }
+}
