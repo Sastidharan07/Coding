d09d23ff0e9ea5152ed6bb9c34a3902a61fc0373
Sastidharan T <sastidharan07@gmail.com>
2025-09-08 08:31:53 +0530

    Create ConvertIntegertotheSumofTwoNoZeroIntegers.java

---
+++ September_2025/Day_08/ConvertIntegertotheSumofTwoNoZeroIntegers.java
@@ -0,0 +1,24 @@
+//Question : https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++) {
+            int j = n - i;
+            if (!containsZero(i) && !containsZero(j)) {
+                return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1}; 
+    }
+
+    private boolean containsZero(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) {
+                return true;
+            }
+            num /= 10;
+        }
+        return false;
+        
+        
+    }
+}
