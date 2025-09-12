ec6923b9077dc9007ed255cb566c4f0efb152c9b
Sastidharan T <sastidharan07@gmail.com>
2025-09-12 08:18:25 +0530

    Create VowelsGameinaString.java

---
+++ September_2025/Day_12/VowelsGameinaString.java
@@ -0,0 +1,15 @@
+//Question : https://leetcode.com/problems/vowels-game-in-a-string/description/
+class Solution
+{
+    public boolean doesAliceWin(String s)
+    {
+        for(int i=0, l=s.length(); i<l; i++)
+            switch(s.charAt(i))
+            {
+                case 'a', 'e', 'i', 'o', 'u' -> {return true;}
+                default -> {}
+            }
+
+        return false;
+    }
+}
