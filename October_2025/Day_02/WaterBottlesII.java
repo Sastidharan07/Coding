0933e27f654915d85cbcbd71390cd9e56d886b5f
Sastidharan T <sastidharan07@gmail.com>
2025-10-02 13:24:38 +0530

    Create Day_02/WaterBottlesII.java

---
+++ October_2025/Day_02/WaterBottlesII.java
@@ -0,0 +1,15 @@
+//Question : https://leetcode.com/problems/water-bottles-ii/description/
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottleDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange; // use empties for exchange
+            numExchange++;               // cost of exchange increases
+            bottleDrunk++;               // got one more bottle
+            emptyBottles++;              // the new bottle becomes empty
+        }
+        return bottleDrunk;
+    }
+}
