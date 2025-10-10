34def71dc4a752ca19935fd9fda4eec3ca11853a
Sastidharan T <sastidharan07@gmail.com>
2025-10-10 10:09:00 +0530

    Create TakingMaximumEnergyFromtheMysticDungeon.java

---
+++ October_2025/Day_10/TakingMaximumEnergyFromtheMysticDungeon.java
@@ -0,0 +1,20 @@
+//Question : https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/description/
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int n = energy.length;
+        int[] dp = new int[n];
+        int max = Integer.MIN_VALUE;
+
+        // Traverse from the end because each dp[i] depends on dp[i + k]
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + k < n) {
+                dp[i] = energy[i] + dp[i + k];
+            } else {
+                dp[i] = energy[i];
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
