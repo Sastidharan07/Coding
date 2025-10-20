cd62e865e68452e89e0377efcd5413fd498251fe
Sastidharan T <sastidharan07@gmail.com>
2025-10-20 12:04:00 +0530

    Create Day_20/FinalValueofVariableAfterPerformingOperations.java

---
+++ October_2025/Day_20/FinalValueofVariableAfterPerformingOperations.java
@@ -0,0 +1,14 @@
+//Question : https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String operation: operations){
+            if(operation.charAt(1)=='+'){
+                x+=1;
+            }else{
+                x-=1;
+            }
+        }
+        return x;
+    }
+}
