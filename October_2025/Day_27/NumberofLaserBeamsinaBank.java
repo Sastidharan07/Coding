720184cd7f3c75af2ec5d8f4a2b2c8c7c884577a
Sastidharan T <sastidharan07@gmail.com>
2025-10-27 09:46:03 +0530

    Create Day_27/NumberofLaserBeamsinaBank.java

---
+++ October_2025/Day_27/NumberofLaserBeamsinaBank.java
@@ -0,0 +1,27 @@
+//Question : https://leetcode.com/problems/number-of-laser-beams-in-a-bank/description/
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int row = bank.length;
+        int ans = 0;
+
+    
+        ArrayList<Integer> deviceCount = new ArrayList<>();
+
+        for (int i = 0; i < row; i++) {
+            String curr = bank[i];
+            int count = 0;
+            for (int j = 0; j < curr.length(); j++) {
+                if (curr.charAt(j) == '1') {
+                    count++;
+                }
+            }
+            if (count > 0) deviceCount.add(count);
+        }
+
+        for (int i = 0; i < deviceCount.size() - 1; i++) {
+            ans += deviceCount.get(i) * deviceCount.get(i + 1);
+        }
+
+        return ans;
+    }
+}
