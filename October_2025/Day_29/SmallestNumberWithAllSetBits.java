4577c102a16a58fe0688ce06ca49bf8f233d4f00
Sastidharan T <sastidharan07@gmail.com>
2025-10-29 12:53:53 +0530

    Create Day_29/SmallestNumberWithAllSetBits.java

---
+++ October_2025/Day_29/SmallestNumberWithAllSetBits.java
@@ -0,0 +1,12 @@
+//Question : https://leetcode.com/problems/smallest-number-with-all-set-bits/description/
+class Solution {
+    public int smallestNumber(int n) {
+        int result = 0;
+        int i = 0;
+        while (result < n) {
+            result += Math.pow(2, i);
+            i++;
+        }
+        return result;
+    }
+}
