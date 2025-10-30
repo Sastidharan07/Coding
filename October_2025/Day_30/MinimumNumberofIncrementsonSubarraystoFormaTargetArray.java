7bb0f65e82080e83ac0b39cab18398635a4d53fa
Sastidharan T <sastidharan07@gmail.com>
2025-10-30 14:51:11 +0530

    Create Day_30/MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java

---
+++ October_2025/Day_30/MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
@@ -0,0 +1,15 @@
+//Question : https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/description/
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int n = target.length;
+        int count = target[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (target[i] > target[i - 1]) {
+                count += target[i] - target[i - 1];
+            }
+        }
+        
+        return count;
+    }
+}
