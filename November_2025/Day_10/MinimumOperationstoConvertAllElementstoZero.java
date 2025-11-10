48d90192ee49f1916b1156d4cd1d571858b4d29b
Sastidharan T <sastidharan07@gmail.com>
2025-11-10 10:43:11 +0530

    Create Day_10/MinimumOperationstoConvertAllElementstoZero.java

---
+++ November_2025/Day_10/MinimumOperationstoConvertAllElementstoZero.java
@@ -0,0 +1,17 @@
+//Question : https://leetcode.com/problems/minimum-operations-to-convert-all-elements-to-zero/description/
+class Solution {
+    public int minOperations(int[] nums) {
+        var stack = new int[nums.length + 1];
+        var top = 0;
+        var ans = 0;
+        for (var i = 0; i < nums.length; i++) {
+            while (stack[top] > nums[i]) {
+                top--;
+                ans++;
+            }
+            if (stack[top] != nums[i])
+                stack[++top] = nums[i];
+        }
+        return ans + top;
+    }
+}
