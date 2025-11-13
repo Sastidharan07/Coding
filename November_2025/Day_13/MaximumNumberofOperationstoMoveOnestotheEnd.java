e8811ff2e07dbb3ef00c39abed755d6c3a95c959
Sastidharan T <sastidharan07@gmail.com>
2025-11-13 12:58:00 +0530

    Create Day_13/MaximumNumberofOperationstoMoveOnestotheEnd.java

---
+++ November_2025/Day_13/MaximumNumberofOperationstoMoveOnestotheEnd.java
@@ -0,0 +1,13 @@
+//Question : https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
+class Solution {
+    public int maxOperations(String s) {
+        int res = 0, o = 0, n = s.length();
+        for (int i = 0; i < n; ++i) {
+            o += s.charAt(i) - '0';
+            if (i > 0 && s.charAt(i) < s.charAt(i - 1)) {
+                res += o;
+            }
+        }
+        return res;
+    }
+}
