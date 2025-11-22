de9d1935837920dbcc2520d66dc58802d66145d3
Sastidharan T <sastidharan07@gmail.com>
2025-11-22 17:05:11 +0530

    Update Find MinimumOperationstoMakeAllElementsDivisiblebyThree.java

--- November_2025/Day_22/Find MinimumOperationstoMakeAllElementsDivisiblebyThree.java
+++ November_2025/Day_22/Find MinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -1,13 +1,13 @@
 //Question : https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/
 class Solution {
     public int minimumOperations(int[] nums) {
-        int count = 0;  // → Initialize our operation counter to zero
-        for (int i = 0; i < nums.length; i++) {  // → Go through each number in the array
-            if (nums[i] % 3 != 0) {   // → Check if current number is NOT divisible by 3
-                nums[i] = nums[i] + 1;  // → If not divisible, add 1 to this number
-                count++;  // → Increment our operation counter
+        int count = 0;  
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 3 != 0) {   
+                nums[i] = nums[i] + 1;
+                count++;
             }       
         }
-        return count;  // → Return total number of operations performed
+        return count;
     }
 }
