9e25bd8a2fa60967d47d3bb45388c39efee27bf2
Sastidharan T <sastidharan07@gmail.com>
2025-11-29 17:34:50 +0530

    Add files via upload

---
+++ leetcode problems/MinimumOperationstoMakeArraySumDivisiblebyK.java
@@ -0,0 +1,10 @@
+//Question : https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
+        return sum%k;
+    }
+}
