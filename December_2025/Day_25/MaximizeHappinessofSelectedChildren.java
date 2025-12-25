2e573add7deeedfb5fffcff83bb43a2709efec58
Sastidharan T <sastidharan07@gmail.com>
2025-12-25 09:44:59 +0530

    Create Day_25/MaximizeHappinessofSelectedChildren.java

---
+++ December_2025/Day_25/MaximizeHappinessofSelectedChildren.java
@@ -0,0 +1,16 @@
+//Question : https://leetcode.com/problems/maximize-happiness-of-selected-children/description/
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int count = 0;
+        long res = 0;
+        for(int i=happiness.length-1;i>=happiness.length-k;i--){
+            if(happiness[i] + count > 0){
+                res+=(long)(happiness[i] + count);
+            }
+            count--;
+        }
+
+        return res;
+    }
+}
