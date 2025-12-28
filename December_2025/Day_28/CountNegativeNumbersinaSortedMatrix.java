922c64a97635fb71839141442836db10d1e007d1
Sastidharan T <sastidharan07@gmail.com>
2025-12-28 09:51:58 +0530

    Create Day_28/CountNegativeNumbersinaSortedMatrix.java

---
+++ December_2025/Day_28/CountNegativeNumbersinaSortedMatrix.java
@@ -0,0 +1,14 @@
+//Question : https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int neg=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]<0){
+                    neg++;
+                }
+            }
+        }
+        return neg;
+    }
+}
