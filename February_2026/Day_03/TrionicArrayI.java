4160caea28b96d8753e93a5b928ddeffdfb83f1f
SASTIDHARAN T <sastidharan07@gmail.com>
2026-02-03 06:47:16 +0530

    Create Day_03/TrionicArrayI.java

---
+++ February_2026/Day_03/TrionicArrayI.java
@@ -0,0 +1,36 @@
+//Question : https://leetcode.com/problems/trionic-array-i/description/
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        if(nums.length==3) {
+           return false;
+        }
+        int index = -1;
+        for(int i=0;i<nums.length-2;i++) {
+          if(nums[i]>=nums[i+1]) {
+            index = i;
+            break;
+          }
+        }
+        if(index==0 || index==-1) {
+            return false;
+        }
+
+        for(int i=index;i<nums.length-1;i++) {
+          if(nums[i]<=nums[i+1]) {
+            index = i;
+            break;
+          }
+        }
+
+        if(index==0 || index==-1) {
+            return false;
+        }
+
+        for(int i=index;i<nums.length-1;i++) {
+          if(nums[i]>=nums[i+1]) {
+            return false;
+          }
+        }
+      return true;
+    }
+}
