0c50f49c7dd69eb52d871bdecd3ff3c52e59d1a5
SASTIDHARAN T <sastidharan07@gmail.com>
2026-02-07 10:47:08 +0530

    Create Day_07/MinimumDeletionstoMakeStringBalanced.java

---
+++ February_2026/Day_07/MinimumDeletionstoMakeStringBalanced.java
@@ -0,0 +1,14 @@
+//Question : https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/description/
+class Solution {
+    public int minimumDeletions(String s) {
+        int b_before_a = 0, deletion = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch == 'b') b_before_a += 1;
+            else if(b_before_a > 0 ) {
+                b_before_a -= 1;
+                deletion += 1;
+            }
+        }   
+        return deletion;
+    }
+}
