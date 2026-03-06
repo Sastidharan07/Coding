4e622c0cf26f66a9ca1e2b5fb2cf5f0eabf93595
SASTIDHARAN T <sastidharan07@gmail.com>
2026-03-06 07:40:38 +0530

    Create Day_06/CheckifBinaryStringHasatMostOneSegmentofOnes.java

---
+++ March_2026/Day_06/CheckifBinaryStringHasatMostOneSegmentofOnes.java
@@ -0,0 +1,19 @@
+//Question : https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        boolean seenZero = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '0') {
+                seenZero = true;
+            } 
+            else if (c == '1' && seenZero) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
