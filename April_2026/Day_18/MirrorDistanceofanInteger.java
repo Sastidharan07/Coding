edd5eafd34ee5ffc3828286861dbc1e7db66b980
SASTIDHARAN T <sastidharan07@gmail.com>
2026-04-18 23:44:56 +0530

    Create Day_18/MirrorDistanceofanInteger.java

---
+++ April_2026/Day_18/MirrorDistanceofanInteger.java
@@ -0,0 +1,13 @@
+//Question : https://leetcode.com/problems/mirror-distance-of-an-integer/description/
+
+class Solution {
+    private int revnum(int a) {
+        if (a / 10 == 0) return a ;
+        String s = new StringBuilder(Integer.toString(a)).reverse().toString();
+        return Integer.parseInt(s) ;
+    }
+
+    public int mirrorDistance(int n) {
+        return Math.abs(n - revnum(n)) ;
+    }
+}
