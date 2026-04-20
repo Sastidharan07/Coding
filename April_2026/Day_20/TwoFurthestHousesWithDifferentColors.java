d6bab3f1888e468c667b1cb1b5f2b51c0bea4cbd
SASTIDHARAN T <sastidharan07@gmail.com>
2026-04-20 08:33:22 +0530

    Create TwoFurthestHousesWithDifferentColors.java

---
+++ April_2026/Day_20/TwoFurthestHousesWithDifferentColors.java
@@ -0,0 +1,17 @@
+//Question : https://leetcode.com/problems/two-furthest-houses-with-different-colors/description/
+class Solution {
+    public int maxDistance(int[] colors) {
+        int n = colors.length;
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            if(colors[i] != colors[0])
+                ans = Math.max(ans, i);
+
+            if(colors[i] != colors[n-1])
+                ans = Math.max(ans, n - 1 - i);
+        }
+
+        return ans;
+    }
+}
