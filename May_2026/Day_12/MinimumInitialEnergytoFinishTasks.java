737cbaa8e2d5245f0472ce29e345de67d419d772
SASTIDHARAN T <sastidharan07@gmail.com>
2026-05-12 17:29:45 +0530

    Create Day_12/MinimumInitialEnergytoFinishTasks.java

---
+++ May_2026/Day_12/MinimumInitialEnergytoFinishTasks.java
@@ -0,0 +1,19 @@
+//Question : https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/description/
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+
+        int curr = 0;
+        int ans  = 0;
+        for (int[] task : tasks) {
+            int actual  = task[0];
+            int minimum = task[1];
+            if (curr < minimum) {
+                ans  += (minimum - curr);
+                curr  = minimum;
+            }
+            curr -= actual;
+        }
+        return ans;
+    }
+}
