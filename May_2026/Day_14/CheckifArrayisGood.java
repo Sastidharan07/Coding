1ac850a293c31c230a8c08b5d16f1f0cb2b8c0ce
SASTIDHARAN T <sastidharan07@gmail.com>
2026-05-14 12:16:48 +0530

    Create Day_14/CheckifArrayisGood.java

---
+++ May_2026/Day_14/CheckifArrayisGood.java
@@ -0,0 +1,21 @@
+//Question : https://leetcode.com/problems/check-if-array-is-good/description/
+class Solution {
+    public boolean isGood(int[] nums) {
+        int[] numsCount = new int[nums.length];
+        for (int num : nums) {
+            if (num >= nums.length) {
+                return false;
+            }
+            numsCount[num]++;
+            if(num < nums.length - 1 && numsCount[num] > 1) {
+                return false;
+            }
+        }
+        for (int i = 1; i < numsCount.length; i++) {
+            if(numsCount[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
