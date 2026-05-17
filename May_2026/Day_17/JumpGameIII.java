1d3701ff69fcc641ef298075c48fb6e1a8c37d72
SASTIDHARAN T <sastidharan07@gmail.com>
2026-05-17 08:11:31 +0530

    Create Day_17/JumpGameIII.java

---
+++ May_2026/Day_17/JumpGameIII.java
@@ -0,0 +1,14 @@
+//Question : https://leetcode.com/problems/jump-game-iii/description/
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length || arr[start] < 0) {
+            return false;
+        }
+        if (arr[start] == 0) {
+            return true;
+        }
+        int jumpDistance = arr[start];
+        arr[start] = -1; // Mark as visited
+        return canReach(arr, start + jumpDistance) || canReach(arr, start - jumpDistance);
+    }
+}
