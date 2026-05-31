d7497bb0c1b326e51352c42e4bdc3e43369b1e6b
SASTIDHARAN T <sastidharan07@gmail.com>
2026-05-31 09:28:39 +0530

    Create Day_31/DestroyingAsteroids.java

---
+++ May_2026/Day_31/DestroyingAsteroids.java
@@ -0,0 +1,17 @@
+//Question : https://leetcode.com/problems/destroying-asteroids/description/
+import java.util.Arrays;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+
+        long cur = mass;
+
+        for (int a : asteroids) {
+            if (cur < a) return false;
+            cur += a;
+        }
+
+        return true;
+    }
+}
