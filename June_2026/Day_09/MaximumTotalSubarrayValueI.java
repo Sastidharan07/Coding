4fdd0fad2fe28fa394554c5c73c20d6a09a4aad2
SASTIDHARAN T <sastidharan07@gmail.com>
2026-06-09 19:28:19 +0530

    Create Day_09/MaximumTotalSubarrayValueI.java

---
+++ June_2026/Day_09/MaximumTotalSubarrayValueI.java
@@ -0,0 +1,14 @@
+//Question : https://leetcode.com/problems/maximum-total-subarray-value-i/description/q
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        long mx = 0;
+        long mn = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num > mx) mx = num;
+            if (num < mn) mn = num;
+        }
+
+        return (mx - mn) * 1L * k;
+    }
+}
