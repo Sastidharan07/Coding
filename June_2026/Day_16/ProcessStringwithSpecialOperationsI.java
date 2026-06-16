08303234c4857caa0f0fa66ef5df2283a1300052
SASTIDHARAN T <sastidharan07@gmail.com>
2026-06-16 08:35:26 +0530

    Update and rename SearchinRotatedSortedArray.java to ProcessStringwithSpecialOperationsI.java

---
+++ June_2026/Day_16/ProcessStringwithSpecialOperationsI.java
@@ -0,0 +1,18 @@
+//Question : https://leetcode.com/problems/process-string-with-special-operations-i/description/
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c != '*' && c != '#' && c != '%') {
+                res.append(c);
+            } else if (c == '#') {
+                res.append(res.toString());
+            } else if (c == '%') {
+                res.reverse();
+            } else {
+                if (res.length() > 0) res.deleteCharAt(res.length() - 1);
+            }
+        }
+        return res.toString();
+    }
+}
